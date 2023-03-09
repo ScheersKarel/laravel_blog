4113fb8bcd83f4edd341ac5836ba3ad0f829927f
ScheersKarel <karel.scheers@student.kdg.be>
2023-03-09 10:48:33 +0100

    made contact table

--- database/migrations/2023_03_09_094354_create_contacts_table.php
+++ database/migrations/2023_03_09_094354_create_contacts_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string("password");
-            $table->boolean("active")->default(1);
-            $table->timestamp('created_at')->nullable();
-            
         });
     }
 
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contacts');
     }
 };
