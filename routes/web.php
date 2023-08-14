eb59dc6485fd749ca21a573cd6b5241d9d43d844
ScheersKarel <karel.scheers@student.kdg.be>
2023-08-14 18:25:31 +0200

    beveileging router toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,17 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::middleware('auth')->group(function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/contacts', [App\Http\Controllers\ContactController::class, 'index'])->name('contacts');
 
-Route::get('/contacts', [App\Http\Controllers\ContactController::class, 'index'])->name('contacts');
+    Route::get('/create', [App\Http\Controllers\ContactController::class, 'Add'])->name('create');
+    Route::POST('create', [App\Http\Controllers\ContactController::class, 'AddContact' ]);
 
-Route::get('/create', [App\Http\Controllers\ContactController::class, 'Add'])->name('create');
-Route::POST('create', [App\Http\Controllers\ContactController::class, 'AddContact' ]);
+    Route::get('edit/{id}', [App\Http\Controllers\ContactController::class, 'edit'])->name('edit');
+    Route::post('edit/{id}', [App\Http\Controllers\ContactController::class, 'update'])->name('update');
 
-Route::get('edit/{id}', [App\Http\Controllers\ContactController::class, 'edit'])->name('edit');
-Route::post('edit/{id}', [App\Http\Controllers\ContactController::class, 'update'])->name('update');
-
-Route::get('delete/{id}', [App\Http\Controllers\ContactController::class, 'delete'])->name('delete');
-Route::POST('delete/{id}', [App\Http\Controllers\ContactController::class, 'DeleteContact']);
+    Route::get('delete/{id}', [App\Http\Controllers\ContactController::class, 'delete'])->name('delete');
+    Route::POST('delete/{id}', [App\Http\Controllers\ContactController::class, 'DeleteContact']);
+});
